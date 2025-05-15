b4cdf5db02f3bb5504ebaa0a8b67d9431a6d64a6
hamidashi-chin <take106141@gmail.com>
2025-05-15 21:49:33 +0900

    出力するMarkdownファイルのsuffix追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import path from "path";
 import { loadConfig } from "../config/loadConfig";
 import { readExcelFile } from "./infrastructure/excel/readExcelFile";
+import { DocumentFactory } from "./domain/DocumentFactory";
+import { writeMarkdownToFile } from "./utils/writeMarkdown";
 
 // const sheetId = '1Tb0gZxGOCiBlbFZKZdXgXp5dV-IDtX3J';
 const sheetId = '1U6HQuh5IfwgKzcIe7CdC3GUgDV_MgcCjjy0oYsIKZxY';
@@ -8,9 +11,13 @@ const sheetId = '1U6HQuh5IfwgKzcIe7CdC3GUgDV_MgcCjjy0oYsIKZxY';
 const range = 'sheet01!A1:I30';
 
 async function main() {
-  const config = loadConfig('config.json');
-  const values = readExcelFile('sample.xlsx');
-  console.log('読み取った内容：', values);
+  const config = loadConfig(path.join(__dirname, '../config/config.json'));
+  const values = readExcelFile(path.join(__dirname, '../test/asserts/テスト用excel.xlsx'));
+
+  const doc = DocumentFactory.fromExcel(values, config);
+  const markdown = doc.toMarkdown();
+  
+  writeMarkdownToFile(markdown, doc.competencyName);
 }
 
 main().catch(console.error);
--- src/utils/writeMarkdown.ts
+++ src/utils/writeMarkdown.ts
@@ -3,7 +3,20 @@ import path from "path";
 
 export function writeMarkdownToFile(markdown: string, title: string, outputDir = 'output') {
   const safeTitle = title.replace(/[\\/:*?"<>|]/g, '_');
-  const filePath = path.join(outputDir, `${safeTitle}.md`);
+
+  const now = new Date();
+  const pad = (n: number) => n.toString().padStart(2, '0');
+
+  // suffix用タイムスタンプ文字列作成 `yyyymmdd_his`
+  const timestamp = String(now.getFullYear())
+                  + String(pad(now.getMonth()))
+                  + String(pad(now.getDate()))
+                  + '_'
+                  + String(pad(now.getHours()))
+                  + String(pad(now.getMinutes()))
+                  + String(pad(now.getSeconds()));
+
+  const filePath = path.join(outputDir, `${safeTitle}_${timestamp}.md`);
 
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
